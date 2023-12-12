8e3824b026026c70444ee0937dbadbbe89084ab7
Büşra Zenbilci <88310614+BusraZenbilci@users.noreply.github.com>
2023-12-12 15:13:52 +0300

    Update Lab01.asm

--- lab01/Lab01.asm
+++ lab01/Lab01.asm
@@ -1,27 +1,54 @@
 ORG 100H
 
-   
-MOV CL, N    ;LOOP COUNTER IS N
-MOV CH, 0  
-MOV DI, 2000H   
-MOV DS, DI    
-MOV DL, 01H 
-MOV AX, 0      
+MOV DH, N   ; LOOP COUNTER IS N 
+MOV DI, 2000H
 
-; CONSTRUCT A LOOP FOR N TIMES
+ 
 
-MYLOOP:
+MOV SI, 01H 
+MOV BX, 0                   
 
-MOV [DI], DL
-ADD AL, DL
-INC DL
+MYLOOP:   
+MOV [DI], SI 
+MOV BP,SI
+
+; Print the value in DL
+MOV CX,8  ; Print 8 bits of DL
+PRINT_LOOP2:
+    MOV AH, 2    ; Function code for printing a character
+    MOV DL, '0'  ; Default character is '0'
+    TEST BP, 10000000b  ; Test the leftmost (high) bit of DL.
+    JZ PRINT_ZERO2
+    MOV DL, '1'  ; If the high bit is set, change character to '1'
+PRINT_ZERO2:
+    INT 21h
+    SHL BP, 1  ; Shift to the next bit
+    LOOP PRINT_LOOP2
+MOV AH, 2
+MOV DL,32
+INT 21h
+ADD BX,SI
+INC SI
 INC DI 
+DEC DH
+JZ END:      
+LOOP MYLOOP   
 
-LOOP MYLOOP 
-MOV [DI], AL
- 
+END:
+    MOV [DI], BX
+
+MOV CX,16  ; Print 8 bits of DL
+PRINT_LOOP:
+    MOV AH, 2    ; Function code for printing a character
+    MOV DL, '0'  ; Default character is '0'
+    TEST BX, 1000000000000000b  ; Test the leftmost (high) bit of DL.
+    JZ PRINT_ZERO
+    MOV DL, '1'  ; If the high bit is set, change character to '1'
+PRINT_ZERO:
+    INT 21h
+    SHL BX, 1  ; Shift to the next bit
+    LOOP PRINT_LOOP
+
+
 RET
-N DB  255
-   
- 
-                         
+N DB 5
